9ca089debcbddfbcd6bcf309c4cadbca85fd1328
Felipa Mendez <felipalouise@gmail.com>
2022-02-14 17:27:06 -0600

    formulas rendered

--- src/Components/Formula.js
+++ src/Components/Formula.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import ListItemFormula from './ListItemFormula';
 
 function Formula ({itemCategories}){
+
+
+   const formulas = itemCategories[2]["formulas"]
+    // console.log(formulas)
+    // formulas.map(item => console.log(item))
+
     return (
         <div>
+            {formulas.map(item => {
+                return(
+                    <>
+                        {/* {console.log(item)} */}
+                        <ListItemFormula item={item} />
+                   </>
+                    )
+                })}
             <p>Formula</p>
         </div>
     )
